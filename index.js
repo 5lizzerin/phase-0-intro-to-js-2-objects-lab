5ca12363a53dec9e640bd14561442d908aaf2472
Lizz <lizzcklein@gmail.com>
2023-08-09 10:21:20 -0400

    completed objects lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name: "Lizz",
+    streetAddress: "123 Example Street",
+}
+
+
+function updateEmployeeWithKeyAndValue(employee) {
+    const newEmployee = {...employee};
+    newEmployee.name = "Sam";
+    newEmployee.streetAddress = "11 Broadway";
+    return newEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    delete newEmployee.streetAddress;
+    return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee) {
+    delete employee.name;
+    delete employee.streetAddress;
+    return employee;
+}
+
+
